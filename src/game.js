462f658a045bc676bb28a26b173d8653a6b7e2f8
MdMahbub-h <2021339001@student.sust.edu>
2025-03-15 17:02:40 +0600

    Add files via upload

---
+++ src/game.js
@@ -0,0 +1,41 @@
+// game.js
+import { GameScene } from "./scenes/GameScene.js";
+import { PauseScene } from "./scenes/PauseScene.js";
+import { LoadingScene } from "./scenes/LoadingScene.js";
+import { GameOverScene } from "./scenes/GameOverScene.js";
+import { MenuScene } from "./scenes/MenuScene.js";
+import { GameState } from "./GameState.js";
+import { GameWinScene } from "./scenes/GameWinScene.js";
+
+const config = {
+  type: Phaser.AUTO,
+  width: 768,
+  height: 768,
+  backgroundColor: "#c0dffd",
+  parent: "game-container",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 },
+      debug: false,
+    },
+  },
+  fps: {
+    target: 60,
+    forceSetTimeOut: true,
+  },
+  scene: [
+    LoadingScene,
+    MenuScene,
+    GameScene,
+    PauseScene,
+    GameOverScene,
+    GameWinScene,
+  ],
+};
+// Initialize shared game state
+const gameState = new GameState();
+
+window.onload = () => {
+  const game = new Phaser.Game(config);
+};
---
+++ src/scenes/MenuScene.js
@@ -0,0 +1,35 @@
+import { GameState } from "../GameState.js";
+class MenuScene extends Phaser.Scene {
+  constructor() {
+    super({ key: "MenuScene" });
+  }
+
+  create() {
+    const { width, height } = this.scale;
+    const gameState = new GameState();
+
+    this.add
+      .text(width / 2, height / 3, "Wasteland Racer", {
+        fontSize: "64px",
+        color: "#000",
+      })
+      .setOrigin(0.5);
+
+    this.add.sprite(width / 2, height / 2, "splashScreen");
+
+    const startButton = this.add
+      .text(width / 2, height / 2, "Start Game", {
+        fontSize: "64px",
+        color: "#fff",
+        stroke: "2",
+      })
+      .setOrigin(0.5)
+      .setInteractive();
+
+    startButton.on("pointerdown", () => {
+      this.scene.stop();
+      this.scene.start("GameScene", { gameState });
+    });
+  }
+}
+export { MenuScene };
